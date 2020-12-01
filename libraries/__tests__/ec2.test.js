f62035ea3082848d9f5f238fb89380887eac1bcd
Nanda Hibatullah <nandahibatullah@gmail.com>
2020-12-01 14:45:02 +1300

    check if instance is stopped first when fetching info

--- libraries/__tests__/ec2.test.js
+++ libraries/__tests__/ec2.test.js
@@ -1,13 +1,13 @@
 const AWS = require('aws-sdk');
+
 const EC2 = require('../ec2');
 
 describe('EC2', () => {
   afterEach(() => AWS.clearAllMocks());
-
   describe('describeInstance', () => {
     describe('when the instance is running', () => {
       it('returns a response with instance data', async () => {
-        AWS.spyOn('EC2', 'waitFor').mockReturnValue({
+        const mockWaitFor = AWS.spyOn('EC2', 'waitFor').mockReturnValue({
           promise: () => Promise.resolve({
             Reservations: [
               {
@@ -23,9 +23,15 @@ describe('EC2', () => {
             ],
           }),
         });
+        const state = 'instanceRunning';
+        const params = {
+          InstanceIds: ['123'],
+        };
+        const expectedResult = { state: 'running', ipAddress: '127.0.0.1' };
+        const describeInstanceRunning = await EC2.describeInstance(state, '123');
 
-        const describeInstanceRunning = await EC2.describeInstance('instanceRunning', '123');
-        expect(describeInstanceRunning).toEqual({ state: 'running', ipAddress: '127.0.0.1' });
+        expect(mockWaitFor).toHaveBeenCalledWith(state, params);
+        expect(describeInstanceRunning).toEqual(expectedResult);
       });
     });
   });
--- libraries/ec2.js
+++ libraries/ec2.js
@@ -2,43 +2,74 @@
 const AWS = require('aws-sdk');
 const config = require('config');
 
+const isInstanceStopped = async (ec2Client, params) => {
+  const response = await ec2Client.waitFor('instanceExists', params).promise();
+  const state = response.Reservations[0].Instances[0].State.Name;
+
+  if (state === 'stopped') {
+    return true;
+  }
+
+  return false;
+};
+
 module.exports = {
-  async describeInstance(state, instanceId) {
+  async describeInstance(stateString, instanceId) {
     const params = {
       InstanceIds: [instanceId],
     };
     const ec2Client = new AWS.EC2({
       region: config.get('serverRegion'),
     });
-    const instancesResponse = await ec2Client.waitFor(state, params).promise();
-    const instanceData = instancesResponse.Reservations[0].Instances[0];
 
-    console.log('\nSERVER INSTANCES\n');
-    console.log(`${JSON.stringify(instancesResponse)}`);
-    console.log('\n');
+    let state = '';
+    let ipAddress = '';
+
+    if (await isInstanceStopped(ec2Client, params)) {
+      state = 'stopped';
+    } else {
+      const instancesResponse = await ec2Client.waitFor(stateString, params).promise();
+      const instanceData = instancesResponse.Reservations[0].Instances[0];
+
+      console.log('\nSERVER INSTANCES\n');
+      console.log(`${JSON.stringify(instancesResponse)}`);
+      console.log('\n');
+
+      state = instanceData.State.Name;
+      ipAddress = instanceData.PublicIpAddress;
+    }
 
     return {
-      state: instanceData.State.Name,
-      ipAddress: instanceData.PublicIpAddress,
+      state,
+      ipAddress,
     };
   },
 
-  async describeInstanceStatus(state, instanceId) {
+  async describeInstanceStatus(stateString, instanceId) {
     const params = {
       InstanceIds: [instanceId],
     };
     const ec2Client = new AWS.EC2({
       region: config.get('serverRegion'),
     });
-    const instanceStatusesResponse = await ec2Client.waitFor(state, params).promise();
-    const instanceStatusData = instanceStatusesResponse.InstanceStatuses[0];
 
-    console.log('\nSERVER INSTANCES\n');
-    console.log(`${JSON.stringify(instanceStatusData)}`);
-    console.log('\n');
+    let status = '';
+
+    if (isInstanceStopped(ec2Client, params)) {
+      status = 'stopped';
+    } else {
+      const instanceStatusesResponse = await ec2Client.waitFor(stateString, params).promise();
+      const instanceStatusData = instanceStatusesResponse.InstanceStatuses[0];
+
+      console.log('\nSERVER INSTANCES\n');
+      console.log(`${JSON.stringify(instanceStatusData)}`);
+      console.log('\n');
+
+      status = instanceStatusData.InstanceStatus.Status;
+    }
 
     return {
-      status: instanceStatusData.InstanceStatus.Status,
+      status,
     };
   },
 
